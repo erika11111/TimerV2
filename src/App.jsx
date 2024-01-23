614efc7882dd3b6c1ee42893ed1da09ff3997138
erika11111 <104774933+erika11111@users.noreply.github.com>
2024-01-23 15:17:23 +0100

    Added changes

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useReducer } from "react";
+
+const countReducer = (state, { type }) => {
+  if (type === "START") {
+    return {
+      ...state,
+      isCounting: true,
+    };
+  }
+  if (type === "STOP") {
+    return {
+      ...state,
+      isCounting: false,
+    };
+  }
+  if (type === "RESET") {
+    return {
+      count: 0,
+      isCounting: false,
+    };
+  }
+  if (type === "TICK") {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+  return state;
+};
+
+function setDefaultValue() {
+  const userCount = localStorage.getItem("count");
+  return userCount ? +userCount : 0;
+}
+
+export default function Timer() {
+  const [{ count, isCounting }, dispatch] = useReducer(countReducer, {
+    count: setDefaultValue(),
+    isCounting: false,
+  });
+
+  useEffect(() => {
+    localStorage.setItem("count", count);
+  }, [count]);
+
+  useEffect(() => {
+    let timerId = null;
+    if (isCounting) {
+      timerId = setInterval(() => {
+        dispatch({ type: "TICK" });
+      }, 1000);
+    }
+    return () => {
+      timerId && clearInterval(timerId);
+      timerId = null;
+    };
+  }, [isCounting]);
+
+  return (
+    <div className="timer">
+      <h1>React Timer</h1>
+      <h3>{count}</h3>
+      {!isCounting ? (
+        <button onClick={() => dispatch({ type: "START" })}>Start</button>
+      ) : (
+        <button onClick={() => dispatch({ type: "STOP" })}>Stop</button>
+      )}
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+    </div>
+  );
+}
